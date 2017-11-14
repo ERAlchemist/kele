804c55950204134bc7edee8c4c5b174c70975b36
Eric Albertus <eralchemist@gmail.com>
2017-11-13 21:40:52 -0800

    get_me method added to kele.rb

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,4 +1,5 @@
 require "httparty"
+require "json"
 
 class Kele
   include HTTParty
@@ -7,6 +8,17 @@ class Kele
     response = self.class.post(base_api_endpoint("sessions"), body: { "email": email, "password": password })
     raise InvalidStudentCodeError.new() if response.code == 401
     @auth_token = response["auth_token"]
+  end
+
+  def get_me
+    response = self.class.get(base_api_endpoint("users/me"), headers: { "authorization" => @auth_token })
+    @user_data = JSON.parse(response.body)
+    @user_data.keys.each do |key|
+      self.class.send(:define_method, key.to_sym) do
+        @user_data[key]
+      end
+    end
+    @user_data
   end
 
 	private
